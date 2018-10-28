f0e8ba9bb36507b39e9f3fa3e126762913625cad
vldrndlek <vldrndlek@gmail.com>
2018-10-28 19:17:45 +0900

    여러줄 쳐지는 문제 수정

--- newomok/Form1.Designer.vb
+++ newomok/Form1.Designer.vb
@@ -23,10 +23,10 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtID = New System.Windows.Forms.RichTextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.txtPass = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -37,66 +37,72 @@ Partial Class frmMain
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.txtID)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(-19, 154)
+        Me.Panel1.Location = New System.Drawing.Point(-22, 192)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(480, 462)
+        Me.Panel1.Size = New System.Drawing.Size(549, 578)
         Me.Panel1.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.PeachPuff
-        Me.Label1.Location = New System.Drawing.Point(31, 124)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(31, 25)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "ID"
-        '
-        'txtID
-        '
-        Me.txtID.BackColor = System.Drawing.Color.SandyBrown
-        Me.txtID.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.txtID.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.txtID.Location = New System.Drawing.Point(36, 152)
-        Me.txtID.Name = "txtID"
-        Me.txtID.Size = New System.Drawing.Size(417, 46)
-        Me.txtID.TabIndex = 3
-        Me.txtID.Text = ""
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.Label2.ForeColor = System.Drawing.Color.PeachPuff
-        Me.Label2.Location = New System.Drawing.Point(31, 230)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(92, 25)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Password"
         '
         'txtPass
         '
         Me.txtPass.BackColor = System.Drawing.Color.SandyBrown
         Me.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtPass.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.txtPass.Location = New System.Drawing.Point(36, 258)
+        Me.txtPass.Location = New System.Drawing.Point(41, 322)
+        Me.txtPass.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPass.Multiline = False
         Me.txtPass.Name = "txtPass"
-        Me.txtPass.Size = New System.Drawing.Size(417, 46)
+        Me.txtPass.Size = New System.Drawing.Size(477, 58)
         Me.txtPass.TabIndex = 5
         Me.txtPass.Text = ""
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Label2.Location = New System.Drawing.Point(35, 288)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 32)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Password"
+        '
+        'txtID
+        '
+        Me.txtID.BackColor = System.Drawing.Color.SandyBrown
+        Me.txtID.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtID.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.txtID.Location = New System.Drawing.Point(41, 190)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtID.Multiline = False
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(477, 58)
+        Me.txtID.TabIndex = 3
+        Me.txtID.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Label1.Location = New System.Drawing.Point(35, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 32)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "ID"
+        '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Silver
-        Me.ClientSize = New System.Drawing.Size(441, 596)
+        Me.ClientSize = New System.Drawing.Size(502, 735)
         Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(457, 635)
+        Me.MaximumSize = New System.Drawing.Size(520, 782)
         Me.MinimizeBox = False
-        Me.MinimumSize = New System.Drawing.Size(457, 635)
+        Me.MinimumSize = New System.Drawing.Size(520, 782)
         Me.Name = "frmMain"
         Me.ShowIcon = False
         Me.Panel1.ResumeLayout(False)
