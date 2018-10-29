64b02de659c92b854eda1fe60d6906995fc95c62
wjswkdlek <44542764+wjswkdlek@users.noreply.github.com>
2018-10-29 22:32:23 +0900

    디자인 변경

--- newomok/Form1.Designer.vb
+++ newomok/Form1.Designer.vb
@@ -23,36 +23,64 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnLogin = New System.Windows.Forms.Button()
         Me.txtPass = New System.Windows.Forms.RichTextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtID = New System.Windows.Forms.RichTextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.BackColor = System.Drawing.Color.Chocolate
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Panel2)
         Me.Panel1.Controls.Add(Me.txtPass)
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.txtID)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(-22, 192)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Panel1.Location = New System.Drawing.Point(-19, -15)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(549, 578)
+        Me.Panel1.Size = New System.Drawing.Size(480, 631)
         Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(152, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.btnLogin)
+        Me.Panel2.Location = New System.Drawing.Point(31, 488)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(417, 58)
+        Me.Panel2.TabIndex = 6
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(152, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.btnLogin.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogin.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.btnLogin.Location = New System.Drawing.Point(3, 3)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(411, 52)
+        Me.btnLogin.TabIndex = 0
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = False
         '
         'txtPass
         '
-        Me.txtPass.BackColor = System.Drawing.Color.SandyBrown
+        Me.txtPass.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtPass.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.txtPass.Location = New System.Drawing.Point(41, 322)
-        Me.txtPass.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPass.Location = New System.Drawing.Point(31, 401)
         Me.txtPass.Multiline = False
         Me.txtPass.Name = "txtPass"
-        Me.txtPass.Size = New System.Drawing.Size(477, 58)
+        Me.txtPass.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.txtPass.Size = New System.Drawing.Size(417, 46)
         Me.txtPass.TabIndex = 5
         Me.txtPass.Text = ""
         '
@@ -60,23 +88,23 @@ Partial Class frmMain
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.Label2.ForeColor = System.Drawing.Color.PeachPuff
-        Me.Label2.Location = New System.Drawing.Point(35, 288)
+        Me.Label2.ForeColor = System.Drawing.Color.Gray
+        Me.Label2.Location = New System.Drawing.Point(31, 373)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(115, 32)
+        Me.Label2.Size = New System.Drawing.Size(92, 25)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Password"
         '
         'txtID
         '
-        Me.txtID.BackColor = System.Drawing.Color.SandyBrown
+        Me.txtID.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.txtID.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtID.Font = New System.Drawing.Font("굴림", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.txtID.Location = New System.Drawing.Point(41, 190)
-        Me.txtID.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtID.Location = New System.Drawing.Point(31, 295)
         Me.txtID.Multiline = False
         Me.txtID.Name = "txtID"
-        Me.txtID.Size = New System.Drawing.Size(477, 58)
+        Me.txtID.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.txtID.Size = New System.Drawing.Size(417, 46)
         Me.txtID.TabIndex = 3
         Me.txtID.Text = ""
         '
@@ -84,29 +112,40 @@ Partial Class frmMain
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("맑은 고딕", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.PeachPuff
-        Me.Label1.Location = New System.Drawing.Point(35, 155)
+        Me.Label1.ForeColor = System.Drawing.Color.Gray
+        Me.Label1.Location = New System.Drawing.Point(31, 267)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 32)
+        Me.Label1.Size = New System.Drawing.Size(31, 25)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "ID"
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.newomok.My.Resources.Resource1.omok
+        Me.PictureBox1.Location = New System.Drawing.Point(113, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 250)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Silver
-        Me.ClientSize = New System.Drawing.Size(502, 735)
+        Me.ClientSize = New System.Drawing.Size(441, 594)
         Me.Controls.Add(Me.Panel1)
-        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(520, 782)
+        Me.MaximumSize = New System.Drawing.Size(457, 633)
         Me.MinimizeBox = False
-        Me.MinimumSize = New System.Drawing.Size(520, 782)
+        Me.MinimumSize = New System.Drawing.Size(457, 633)
         Me.Name = "frmMain"
         Me.ShowIcon = False
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -116,4 +155,7 @@ Partial Class frmMain
     Friend WithEvents txtPass As RichTextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents txtID As RichTextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- newomok/Form1.vb
+++ newomok/Form1.vb
@@ -1,2 +1,16 @@
 ﻿Public Class frmMain
+    Private Function UnicodeBytesToString(
+    ByVal bytes() As Byte) As String
+        Return System.Text.Encoding.Unicode.GetString(bytes)
+    End Function
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim Winhttp As New WinHttp.WinHttpRequest
+        Dim id As String
+        Dim pass As String
+        Winhttp.Open("GET", "http://changhyun.dothome.co.kr/login.txt")
+        Winhttp.Send()
+        Winhttp.WaitForResponse()
+
+        MsgBox(UnicodeBytesToString(Winhttp.ResponseBody))
+    End Sub
 End Class
